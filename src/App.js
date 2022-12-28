93182bdef9f1be2fc5c906cfe1cfeab02add8922
shahgee77 <hashmigee96@gmail.com>
2022-12-28 19:56:44 +0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
 
 function App() {
-  return <h1 className="bg-red-400">Hello Word</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forget-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
