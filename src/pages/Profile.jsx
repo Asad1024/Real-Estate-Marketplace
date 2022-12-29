c0b964de9207162d3fa3235d39f0694c5ece784d
shahgee77 <hashmigee96@gmail.com>
2022-12-29 17:37:35 +0500

    create private route and custom hook

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className={
+                "mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              }
+            />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className={
+                "mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+              }
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  Edit
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Profile;
