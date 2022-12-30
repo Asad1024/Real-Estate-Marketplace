915762daadb88f528817580d9bdaaa1faa3f1482
shahgee77 <hashmigee96@gmail.com>
2022-12-30 17:46:11 +0500

    Create Contact Functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -21,6 +21,7 @@ import {
   FaParking,
   FaChair,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 const Listing = () => {
   const auth = getAuth();
@@ -132,6 +133,19 @@ const Listing = () => {
               {listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
         </div>
         <div></div>
       </div>
