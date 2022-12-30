5a011691495fbc368474ae937023bc4cc22134f0
shahgee77 <hashmigee96@gmail.com>
2022-12-30 19:22:58 +0500

    Rent and Sale Pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
-// import Category from "./pages/Category";
+import Category from "./pages/Category";
 function App() {
   return (
     <>
@@ -31,7 +31,7 @@ function App() {
             element={<Listing />}
           />
           <Route path="/offers" element={<Offers />} />
-          {/* <Route path="/category/:categoryName" element={<Category />} /> */}
+          {<Route path="/category/:categoryName" element={<Category />} />}
           <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
