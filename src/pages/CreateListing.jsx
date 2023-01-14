c97a9ed2904175e8e8b6b8ffb363461aa47d1961
Asad Shah <116341539+Asad1024@users.noreply.github.com>
2023-01-14 18:12:20 +0500

    Update CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -353,7 +353,7 @@ export default function CreateListing() {
           id="contact"
           value={contact}
           onChange={onChange}
-          placeholder="Description"
+          placeholder="+92"
           required
           className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
         />
