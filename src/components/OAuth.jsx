1d5eb733389c68958e88093fc17f44df71cf92fc
Asad Shah <116341539+Asad1024@users.noreply.github.com>
2023-01-16 22:57:51 +0500

    Update OAuth.jsx

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -4,7 +4,11 @@ import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../App";
+
 export default function OAuth() {
+  const darkMode = useContext(DarkModeContext);
   const navigate = useNavigate();
   async function onGoogleClick() {
     try {
@@ -33,7 +37,9 @@ export default function OAuth() {
     <button
       type="button"
       onClick={onGoogleClick}
-      className="flex items-center justify-center w-full bg-white px-7 py-3 text-md font-medium hover:bg-orange-800 hover:text-white active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+      className={`flex items-center justify-center w-full bg-white px-7 py-3 text-md font-medium hover:bg-orange-800 hover:text-white active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded ${
+        darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-800"
+      }`}
     >
       <FcGoogle className=" bg-white rounded-full mr-2" />
       Sign in with Google
