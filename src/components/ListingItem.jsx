93a3ee5ee97ffea81096e93f54477edcf0de4799
Asad Shah <116341539+Asad1024@users.noreply.github.com>
2023-01-16 22:21:54 +0500

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 import { FaTrash } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+import { DarkModeContext } from "../App";
+import { useContext } from "react";
 export default function ListingItem({ listing, id, onEdit, onDelete }) {
+  const darkMode = useContext(DarkModeContext);
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -18,15 +21,21 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
         >
           {listing.timestamp?.toDate()}
         </Moment>
-        <div className="w-full p-[10px]">
+        <div
+          className={`w-full p-[10px] ${
+            darkMode
+              ? "text-indigo-100 bg-gray-800"
+              : "text-indigo-900 bg-indigo-100"
+          }`}
+        >
           <div className="flex items-center space-x-1">
             <MdLocationOn className="h-4 w-4 text-green-600" />
-            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
+            <p className="font-semibold text-sm mb-[2px] truncate">
               {listing.address}
             </p>
           </div>
           <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
-          <p className="text-[#457b9d] mt-2 font-semibold">
+          <p className="text-indigo-500 mt-2 font-semibold">
             Rs{" "}
             {listing.offer
               ? listing.discountedPrice
