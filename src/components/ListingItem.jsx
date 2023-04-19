afd39fb78266414181d6f42ab2af55581fcf2185
Asad Shah <116341539+Asad1024@users.noreply.github.com>
2023-04-19 23:33:24 +0500

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,8 +5,14 @@ import { FaTrash } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 import { DarkModeContext } from "../App";
 import { useContext } from "react";
+import { getAuth } from "firebase/auth";
 export default function ListingItem({ listing, id, onEdit, onDelete }) {
   const darkMode = useContext(DarkModeContext);
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+  } else {
+  }
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
       <Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -21,7 +27,9 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
         >
           {listing.timestamp?.toDate()}
         </Moment>
-        <div
+        {user ? (
+    <>
+       <div
           className={`w-full p-[10px] ${
             darkMode
               ? "text-indigo-100 bg-gray-800"
@@ -61,6 +69,22 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
             </div>
           </div>
         </div>
+    </>
+  ) : (
+    <div className="w-full text-center bg-white p-6 rounded-lg shadow-lg">
+    <p className="text-xl font-semibold mb-3">
+      To view details, please log in.
+    </p>
+    <Link
+      to="/sign-in"
+      className="bg-blue-600 text-white py-3 px-6 rounded-md shadow-md hover:bg-blue-700 focus:bg-blue-700 transition duration-150 ease-in-out"
+    >
+      Sign In
+    </Link>
+  </div>
+  
+  )}
+
       </Link>
       {onDelete && (
         <FaTrash
